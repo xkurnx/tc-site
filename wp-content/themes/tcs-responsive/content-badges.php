7696d301c5c14d2ca14ab18b8e2d5c6fca3090cf
mashannon168 <sma@appirio.com>
2014-01-08 14:24:45 -0600

    Synched with staging.

---
+++ wp-content/themes/tcs-responsive/content-badges.php
@@ -0,0 +1,50 @@
+<div class="badgeGroups">
+<!-- <?php echo get_basic_achievements_shortcode(array("width" => 230), get_query_var( 'handle' )); ?>
+-->
+	<div class="groupBadge Forum-Posts" style="">
+		<span class="subBadge Forum-Posts-1 selected"></span><span class="subBadge Forum-Posts-100 selected"></span><span class="subBadge Forum-Posts-500 selected"></span><span class="subBadge Forum-Posts-1000 selected"></span><span class="subBadge Forum-Posts-5000"></span>
+	</div>			
+	<div class="groupBadge Marathon-Matches" style="">
+		<span class="subBadge Marathon-Matches-1 selected"></span><span class="subBadge Marathon-Matches-3"></span><span class="subBadge Marathon-Matches-10"></span><span class="subBadge Marathon-Matches-20"></span><span class="subBadge Marathon-Matches-50"></span>
+	</div>
+	<div class="groupBadge Marathon-Top-5-Placements" style="display: none;">
+		<span class="subBadge Marathon-Top-5-Placements-1"></span><span class="subBadge Marathon-Top-5-Placements-2"></span><span class="subBadge Marathon-Top-5-Placements-4"></span><span class="subBadge Marathon-Top-5-Placements-8"></span><span class="subBadge Marathon-Top-5-Placements-16"></span>
+	</div>
+	<div class="groupBadge Passing-Submissions" style="">
+		<span class="subBadge Passing-Submissions-1 selected"></span><span class="subBadge Passing-Submissions-50 selected"></span><span class="subBadge Passing-Submissions-100 selected"></span><span class="subBadge Passing-Submissions-250"></span><span class="subBadge Passing-Submissions-500"></span>
+	</div>
+	<div class="groupBadge Checkpoint-Prizes" style="">
+		<span class="subBadge Checkpoint-Prizes-1 selected"></span><span class="subBadge Checkpoint-Prizes-50"></span><span class="subBadge Checkpoint-Prizes-100"></span><span class="subBadge Checkpoint-Prizes-250"></span><span class="subBadge Checkpoint-Prizes-500"></span>
+	</div>
+	<div class="groupBadge Winning-Placements" style="">
+		<span class="subBadge Winning-Placements-1 selected"></span><span class="subBadge Winning-Placements-25 selected"></span><span class="subBadge Winning-Placements-50 selected"></span><span class="subBadge Winning-Placements-100"></span><span class="subBadge Winning-Placements-250"></span>
+	</div>
+	<div class="groupBadge First-Place-Wins" style="">
+		<span class="subBadge First-Place-Wins-1 selected"></span><span class="subBadge First-Place-Wins-25 selected"></span><span class="subBadge First-Place-Wins-50"></span><span class="subBadge First-Place-Wins-100"></span><span class="subBadge First-Place-Wins-250"></span>
+	</div>
+	<div class="clear-float"></div>
+
+
+</div>
+<!-- /.badgeGroups -->
+
+<div class="footer-badges">
+	<div class="singleBadge Successful-Challenges-200 "></div>
+	<div class="singleBadge Solved-Hard-Div2-Problem-in-SRM"></div>
+	<div class="singleBadge Solved-Hard-Div1-Problem-in-SRM"></div>
+	<div class="singleBadge Algorithm-Target "></div>
+	<div class="singleBadge SRM-Winner-Div-2"></div>
+	<div class="singleBadge SRM-Winner-Div-1"></div>
+	<div class="singleBadge Algorithm-Target "></div>
+	<div class="singleBadge SRM-Winner-Div-2"></div>
+	<div class="singleBadge SRM-Winner-Div-1"></div>
+	<div class="singleBadge Algorithm-Target "></div>
+	<div class="singleBadge SRM-Winner-Div-2"></div>
+	<div class="singleBadge SRM-Winner-Div-1"></div>
+	<div class="singleBadge SRM-Winner-Div-1"></div>
+	<div class="singleBadge Algorithm-Target "></div>
+	<div class="singleBadge studio-cup-winner"></div>
+	<div class="singleBadge studio-cup-top-5"></div>
+	<div class="clear-float"></div>
+</div>
+<!-- /.footer-badges -->
