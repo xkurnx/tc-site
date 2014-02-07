c7d463b752aabaa965610158b77b1f951ce511f3
ajefts <ajefts@users.noreply.github.com>
2014-02-06 23:19:54 -0500

    Added Downloads list for development challenges

--- wp-content/themes/tcs-responsive/page-challenge-details.php
+++ wp-content/themes/tcs-responsive/page-challenge-details.php
@@ -1040,6 +1040,22 @@ $registrants = $contest->registrants;
 										<?php 
 										if ( $contestType != 'design' ):
 										?>
+							               		     <h3>Downloads:</h3>                                
+							                   	  	<div class="inner">
+														<?php 
+															echo '<ul>';
+															if (!empty($contest->Documents)){
+																foreach( $contest->Documents as $value ){
+																	$document = $value;
+																	echo '<li><a href="' . $document->url . '">' . $document->documentName . '</a></li>';
+																}
+															} else {
+																echo '<li><strong>None</li></strong>';
+															}
+															echo '</ul>';
+														?>  
+														                   	  	
+							                   	  	</div>										
 										<li class="slide">
 											 
                                              <div class="reviewStyle slideBox">
